8a4ef7c61600e0e425d9fdc91db15e53ffe604f2
Geofat <geofat2015@gmail.com>
2022-10-26 19:44:55 +0100

    added hamburger menu

---
+++ main.js
@@ -0,0 +1,7 @@
+
+var hamburger = document.querySelector('.hamburger')
+var links =document.querySelector('.mobilelink-holder')
+function addclass() {
+    hamburger.classList.toggle('showlink')
+    links.classList.toggle('showmobilelink')
+}
